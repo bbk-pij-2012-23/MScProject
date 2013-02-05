6c43546988fd855a884343d4a9bce8eef5e1b390
emann06 <eleanor.mann@bbk.com>
2013-02-05 16:43:54 +0000

    airbus added

--- OODAPSession5/src/test/AirplaneTest.java
+++ OODAPSession5/src/test/AirplaneTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import vehicles.Airbus;
 import vehicles.Airplane;
 import vehicles.FFJ;
 import vehicles.Flying;
@@ -43,11 +44,20 @@ public class AirplaneTest {
 	public void test3(){
 		String expectedOutput = "I don't Fly";
 		String stringReturned = null;
-		
 		Flying fly = new ModelAirPlane();
 		Airplane classUnderTest = new Airplane(1,fly);
 		stringReturned = classUnderTest.howDoYouFly();
 		assertEquals("Wrong answer!", stringReturned, expectedOutput);
+	}
+	
+	@Test
+	public void test4(){
+		String expectedOutput = "With lots of fuel";
+		String stringReturned = null;
+		Flying fly = new Airbus();
+		Airplane classUnderTest = new Airplane(1,fly);
+		stringReturned = classUnderTest.howDoYouFly();
+		assertEquals("Wrong answer!", stringReturned, expectedOutput); 
 	}
 }
 
---
+++ OODAPSession5/src/vehicles/Airbus.java
@@ -0,0 +1,10 @@
+package vehicles;
+
+public class Airbus implements Flying {
+
+	@Override
+	public String howIFly() {
+		return "With lots of fuel";
+	}
+
+}
