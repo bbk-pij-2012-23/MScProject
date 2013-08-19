82335f30478cafc0a5e6269ac952f0b3869f36a4
emann06 <eleanor.mann@bbk.com>
2013-08-19 20:09:45 +0100

    end of day

---
+++ AnalysisBBK/src/analysis/Output.java
@@ -0,0 +1,13 @@
+package analysis;
+
+public class Output {
+	private double[] summary; //frequency, meanDuration, maxIncrease...
+	private double[][] data; //matrix of statistics about each HF event;hf1 - time (serial), magnitude, start, end, duration, validated
+	
+	/*needs some settings gathering method;
+	 * 
+	 */
+	
+	
+	
+}
